582880a6a1c0729a67e0a8eabf051809662e0f59
Keiner Mendoza <keinermendoza@gmail.com>
2025-10-08 23:46:13 -0400

    fix: make and apply migration for TemplateGenerator model
    - Register TemplateGenerator in admin panel
    - Move partial templates folder directly under the templates root directory

--- site_generator/admin.py
+++ site_generator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TemplateGenerator
 
-# Register your models here.
+@admin.register(TemplateGenerator)
+class TemplateGeneratorAdmin(admin.ModelAdmin):
+    list_display = ['project_name']
+
+    prepopulated_fields = {
+        'slug': ('project_name',)
+    }
