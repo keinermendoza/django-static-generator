be9554387a5ea0b6a907688cebe809aa4d01c094
Keiner Mendoza <keinermendoza@gmail.com>
2025-10-16 13:47:27 -0400

    fix: display only one svg at a time in password visibility button

--- mywidgets/static/mywidgets/js/custom_password.js
+++ mywidgets/static/mywidgets/js/custom_password.js
@@ -3,12 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
     Array.from(inputContainers).forEach(container => {
         const input =  container.querySelector('.custom-password-input');
         const button =  container.querySelector('.btn-toogle-text');
+        const svgShow = container.querySelector('.pasword-hidden');
+        const svgHide = container.querySelector('.password-showed');
+        
+        // hide svg on init
+        svgHide.style.display = "none";
+        button.style.display = "grid";
 
         button.onclick = () => {
             button.classList.toggle("password-is-visible")
             if (input.type === "password") {
                 input.type = "text"
+
+                svgShow.style.display = "none";
+                svgHide.style.display = "block";
+
             } else {
+                svgShow.style.display = "block";
+                svgHide.style.display = "none";
+
                 input.type = "password"
             }
         } 
